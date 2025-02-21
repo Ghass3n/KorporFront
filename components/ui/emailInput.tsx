8360f7bc506b9e19a30fb3d2431bdb9731cbdafc
Ghass3n <hmidamohamedghassen@gmail.com>
2025-02-21 13:55:11 +0100

    Updated the structure of the project to accomodate for the api backend

--- components/ui/emailInput.tsx
+++ components/ui/emailInput.tsx
@@ -6,20 +6,26 @@ const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledTextInput = styled(TextInput);
 
-export default function Input({ placeholder }: { placeholder: string }) {
-  const [email, setEmail] = useState("");
+export default function EmailInput({
+  placeholder,
+  value,
+  onChangeText,
+}: {
+  placeholder: string;
+  value: string;
+  onChangeText: (text: string) => void;
+}) {
   const [isValid, setIsValid] = useState(true);
 
   const validateEmail = (text: string) => {
-    // Regex to check if the email is valid
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    setIsValid(text === "" || emailRegex.test(text)); // Allow empty input
+    setIsValid(text === "" || emailRegex.test(text));
   };
 
   return (
     <StyledView className="w-full">
       <StyledTextInput
-        className={`border rounded-lg px-4 h-12 text-base w-full py-3  ${
+        className={`border rounded-lg px-4 h-12 text-base w-full py-3 ${
           isValid
             ? "border-gray-300 bg-[#F0F4FA] mb-3"
             : "border-red-500 bg-red-100"
@@ -29,9 +35,12 @@ export default function Input({ placeholder }: { placeholder: string }) {
         keyboardType="email-address"
         autoCapitalize="none"
         autoCorrect={false}
-        value={email}
-        onChangeText={(text) => setEmail(text)} // Updates state but doesn't validate immediately
-        onBlur={() => validateEmail(email)} // Validate only when input loses focus
+        value={value}
+        onChangeText={(text) => {
+          onChangeText(text);
+          validateEmail(text);
+        }}
+        onBlur={() => validateEmail(value)}
       />
       {!isValid && (
         <StyledText className="text-red-500 text-sm mb-1">
